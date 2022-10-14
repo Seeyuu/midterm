0172733663ae9dd31b26067aa78da20b76f9b702
Phetcharaphon456 <110883429+Seeyuu@users.noreply.github.com>
2022-10-14 11:57:07 +0700

    Add files via upload

---
+++ test/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+enum Menu
+{
+    Register = 1,
+    AttendantList = 2,
+    Login = 3
+}
+class Program
+{
+    
+    public static void Main(string[] args)
+    {
+        PrintMenuScreen();
+        InputMenuFromKeyboard();
+
+     
+    }
+    public static void PrintMenuScreen()
+    {
+        Console.Clear();
+        PrintListMenu();
+        
+        
+    }
+    public static void PrintListMenu()
+    {
+        Console.WriteLine("Welcome to Idea Camp 2022.");
+        Console.WriteLine("***************************************************");
+        Console.WriteLine("1.Register");
+        Console.WriteLine("2.Attendant Lists");
+        Console.WriteLine("3.Login");
+        Console.WriteLine("***************************************************");
+    }
+    public static void InputMenuFromKeyboard()
+    {
+        Console.Write("Please Input Menu: ");
+        Menu menu = (Menu)(int.Parse(Console.ReadLine()));
+
+    }
+}
